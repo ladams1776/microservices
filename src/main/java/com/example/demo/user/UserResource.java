05a6008835d80300ba111406103e0501d8b17ce7
larry.adams <larry.adams@echoman.com>
2019-09-02 10:39:39 -0400

    Add Swagger Api annotations
    Adds more details to the Swagger url

--- src/main/java/com/example/demo/user/UserResource.java
+++ src/main/java/com/example/demo/user/UserResource.java
@@ -1,8 +1,15 @@
 package com.example.demo.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,13 +18,19 @@ import java.util.stream.Stream;
 public class UserResource {
 
     //@TODO: Lazy atm, move this out at some point.
+    @ApiModel(description = "User Resource - tells us everything about Users")
     class User {
+
         private String firstName;
         private String lastName;
+        @Past
+        @ApiModelProperty(notes="Birth date should be in the past")
+        private Date birthDate;
 
-        public User(String firstName, String lastName) {
+        public User(String firstName, String lastName, Date birthDate) {
             this.firstName = firstName;
             this.lastName = lastName;
+            this.birthDate = birthDate;
         }
 
         public String getFirstName() {
@@ -35,6 +48,14 @@ public class UserResource {
         public void setLastName(String lastName) {
             this.lastName = lastName;
         }
+
+        public Date getBirthDate() {
+            return birthDate;
+        }
+
+        public void setBirthDate(Date birthDate) {
+            this.birthDate = birthDate;
+        }
     }
 
     @RequestMapping(path = "/getUsers")
@@ -42,7 +63,7 @@ public class UserResource {
         final int[] i = {1};
         final List<User> some = Stream.generate(() -> {
             i[0] += 1;
-            return new User(String.valueOf(i[0]), "adams");
+            return new User(String.valueOf(i[0]), "adams", new Date());
         })
                 .limit(5)
                 .collect(Collectors.toList());
