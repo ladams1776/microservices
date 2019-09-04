bfacfa27552e5c4a4a3cd5a57fe0068cd0db2c40
larry.adams <larry.adams@echoman.com>
2019-09-04 07:55:54 -0400

    Watching Jackson filtering apply to a list of beans.

--- src/main/java/com/example/demo/FilteringController.java
+++ src/main/java/com/example/demo/FilteringController.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 /**
  * experimenting with property filtering
  */
@@ -10,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class FilteringController {
 
     @GetMapping("/filtering")
-    public SomeBean retrieveSomeBean() {
-        return new SomeBean("value1", "value2", "value2");
+    public List<SomeBean> retrieveSomeBean() {
+        return asList(
+                new SomeBean("value1", "value2", "value2"),
+                new SomeBean("value2", "value3", "value4")
+        );
     }
 }
