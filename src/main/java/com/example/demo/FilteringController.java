cdb5cdc9f3dd96c8c829dba80a123b3ae269692c
larry.adams <ladams1776@yahoo.com>
2019-09-04 08:10:16 -0400

    Swap over to dynamic filtering

--- src/main/java/com/example/demo/FilteringController.java
+++ src/main/java/com/example/demo/FilteringController.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-import static java.util.Arrays.asList;
 
 /**
  * experimenting with property filtering
@@ -14,11 +14,16 @@ import static java.util.Arrays.asList;
 public class FilteringController {
 
     @GetMapping("/filtering")
-    public List<SomeBean> retrieveSomeBean() {
-        return asList(
-                new SomeBean("value1", "value2", "value2"),
-                new SomeBean("value2", "value3", "value4")
-        );
+    public MappingJacksonValue retrieveSomeBean() {
+        final SomeBean someBean = new SomeBean("value1", "value2", "value2");
 
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 }
--- src/main/java/com/example/demo/SomeBean.java
+++ src/main/java/com/example/demo/SomeBean.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
-@JsonIgnoreProperties(value={"field1", "field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
     private String field2;
