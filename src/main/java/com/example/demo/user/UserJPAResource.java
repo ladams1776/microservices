91a6461994db2946cb36dd4e6cf0129f585ebf75
larry.adams <ladams1776@yahoo.com>
2019-09-07 10:08:23 -0400

    Post for a new user

--- src/main/java/com/example/demo/user/UserJPAResource.java
+++ src/main/java/com/example/demo/user/UserJPAResource.java
@@ -2,9 +2,12 @@ package com.example.demo.user;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -38,4 +41,8 @@ public class UserJPAResource {
         this.userRepository.deleteById(id);
     }
 
+    @PostMapping(path="/jpa/users")
+    public User createUser(@Valid @RequestBody User user) {
+        return this.userRepository.save(user);
+    }
 }
