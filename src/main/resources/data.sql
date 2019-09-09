57cd2131808ca64eb440be834729254f9e446d9e
larry.adams <ladams1776@yahoo.com>
2019-09-09 06:17:51 -0400

    fix script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ insert into user values(200, sysdate(), 'Jill');
 insert into user values(300, sysdate(), 'Jam');
 
 insert into post values(100, 'My first post', 100);
-insert into post values(100, 'My second post post', 100);
-insert into post values(101, 'My first post', 200);
-insert into post values(101, 'My second post', 200);
-insert into post values(102, 'My first post', 300);
-insert into post values(102, 'My second post', 300);
+insert into post values(101, 'My second post post', 100);
+insert into post values(102, 'My first post', 200);
+insert into post values(103, 'My second post', 200);
+insert into post values(104, 'My first post', 300);
+insert into post values(105, 'My second post', 300);
