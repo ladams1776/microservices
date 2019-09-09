0f85f9f1b6bc403ce94d330514bbaecfc9a628c4
larry.adams <ladams1776@yahoo.com>
2019-09-09 06:12:43 -0400

    Add more data to the script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
 insert into user values(100, sysdate(), 'AB');
 insert into user values(200, sysdate(), 'Jill');
 insert into user values(300, sysdate(), 'Jam');
+
+insert into post values(100, 'My first post', 100);
+insert into post values(100, 'My second post post', 100);
+insert into post values(101, 'My first post', 200);
+insert into post values(101, 'My second post', 200);
+insert into post values(102, 'My first post', 300);
+insert into post values(102, 'My second post', 300);
